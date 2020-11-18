bb6416a5cccac218eb8d25998c50b8e8394f18bf
David <davidferreira.m1@outlook.com>
2020-11-18 14:18:46 -0300

    feat(develop): ajuste das migrations

--- src/database/migrations/20201114051415-create-tables.js
+++ src/database/migrations/20201114051415-create-tables.js
@@ -61,7 +61,7 @@ module.exports = {
       },
       created_at: {
         type: Sequelize.DATE,
-        allowNull: false
+        allowNull: false,
       },
       updated_at: {
         type: Sequelize.DATE,
